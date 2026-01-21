99732726fcf16482d00a283ba3bc123bb22910d6
abdessamad-el <elouadghiri.abdessamad.mp@gmail.com>
2026-01-21 16:33:31 +0100

    create kafka topics config

---
+++ src/main/java/io/trading/platform/kafka/KafkaTopicsProperties.java
@@ -0,0 +1,15 @@
+package io.trading.platform.kafka;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties(prefix = "trading.kafka.topics")
+@Getter
+@Setter
+public class KafkaTopicsProperties {
+	private String marketQuotes = "market.quotes.v1";
+	private String marketMinuteBars = "market.bars.minute.v1";
+}
